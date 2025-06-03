aeb26ca5c6171c7cc31241c0a9cb3c84626912a2
Debashree Jena <jenadeba07@gmail.com>
2025-06-03 10:57:32 +0530

    Counter app create

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,57 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Signup } from './signup/signup';
+import { Login } from './login/login';
+import { ProfileComponent } from './profile/profile';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // imports: [Login, Signup, ProfileComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected title = 'angular-tut';
+  // protected title = 'Debashree';
+  // data: number | string = 30; //declare multiple type
+  // userLogin: boolean = true;
+  // info: number | string | boolean = "this is some data"
+  // info: any = 'this is some data'; //any kind of data we can store
+  // apple() {
+  //   this.title = 'Rossy';
+  //   this.data = 'Name';
+  //   this.info = 30;
+  //   this.info = 'Deba';
+  //   this.info = [1, 2, 3];
+  // }
+  // sum(a: number, b: number) {
+  //   console.log(a + b);
+  // }
+
+  count: number = 0;
+
+  increment() {
+    this.count++;
+  }
+
+  decrement() {
+    this.count = this.count - 1;
+  }
+
+  reset() {
+    this.count = 0;
+  }
+
+  // instead of above three function we used only one function with single parameter
+
+  handleCounter(val: string) {
+    if (val == 'minus') {
+      if (this.count > 0) {
+        this.count--;
+      }
+    } else if (val == 'plus') {
+      this.count++;
+    } else {
+      this.count = 0;
+    }
+  }
 }
