40ccab57d33a712b98a5dc0d5345eade00ae0d8e
Debashree Jena <jenadeba07@gmail.com>
2025-06-07 16:35:59 +0530

    create page not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { Blog } from './blog/blog';
 import { Contact } from './contact/contact';
 import { About } from './about/about';
 import { Home } from './home/home';
+import { PageNotFound } from './page-not-found/page-not-found';
 
 export const routes: Routes = [
+  { path: '', component: Home },
   { path: 'signIn', component: SignIn },
   { path: 'about', component: About },
   { path: 'contact', component: Contact },
   { path: 'blog', component: Blog },
-  { path: '', component: Home },
+  { path: '**', component: PageNotFound },
 ];
